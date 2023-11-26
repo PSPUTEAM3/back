747f40d36ed88db4c125fc03bb6d317ae1eedf5f
KinGNoteBook <taels2011@yandex.ru>
2023-11-26 15:47:57 +0500

    +новости

--- WebApplication3/ApplicationDbContext.cs
+++ WebApplication3/ApplicationDbContext.cs
@@ -9,10 +9,5 @@ public class ApplicationDbContext : DbContext
     }
     public DbSet<ApplicationUser> users { get; set; }
     public DbSet<InvalidToken> InvalidTokens { get; set; }
-    //public DbSet<Post> post { get; set; }
-    //protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //{
-    //    modelBuilder.Entity<InvalidToken>().HasKey(j => j.Id); // Замените 'Id' на имя свойства, которое должно быть первичным ключом
-    //}
-
+    public DbSet<Post> post { get; set; }
 }
